581c2565f46c8ec5b992a5be66321c6ec167d735
Melkamu8 <melkamudi@yahoo.com>
2020-01-09 14:33:04 -0500

    exercise204

---
+++ src/main/java/com/example/demo/SpringBootex204Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootex204Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootex204Application.class, args);
+    }
+
+}
